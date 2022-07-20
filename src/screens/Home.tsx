301857431d751d354f81945f3300ab89c9c6392d
Roberto <roherool@hotmail.com>
2022-07-20 18:22:25 -0300

    Finished styling the order component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,23 +6,20 @@ import {
   useTheme, 
   Text, 
   Heading, 
-  FlatList 
+  FlatList,
+  Center, 
 } from 'native-base';
-import { SignOut } from 'phosphor-react-native';
+import { ChatTeardropText, SignOut } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg'
 
 import { Filter } from '../components/Filter';
 import { Order, OrderProps } from '../components/Order';
+import { Button } from '../components/Button';
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-  const [orders, setOrders] = useState<OrderProps[]>([{
-    id: '123',
-    patrimony:'123456',
-    when: '18/07/2022 às 10:00',
-    status: 'open'
-  }]);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
 
   const { colors } = useTheme();
 
@@ -78,7 +75,25 @@ export function Home() {
           data={orders}
           keyExtractor={item => item.id}
           renderItem={({ item }) => <Order data={item}/>}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text 
+                color="gray.300" 
+                fontSize="xl" 
+                mt={6} 
+                textAlign="center"
+              >
+                Você ainda não possui {'\n'}
+                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+              </Text>
+            </Center>
+          )}
         />
+
+        <Button title="Nova solicitação"/>
       </VStack>
     </VStack>
   );
